b868310ef4e7945ab5fd0ccecec45bbcd9fac668
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-05-14 22:17:44 -0500

    initial commit

---
+++ src/main/java/com/wedevol/springbootjunit4/SpringBootJUnit4Application.java
@@ -0,0 +1,13 @@
+package com.wedevol.springbootjunit4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnit4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJUnit4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedevol/springbootjunit4/SpringBootJUnit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedevol.springbootjunit4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJUnit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
