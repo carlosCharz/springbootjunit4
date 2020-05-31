82293b284b9b049783d761484ccd650657b6a4aa
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-05-30 20:19:03 -0500

    use junit 4

--- src/test/java/com/wedevol/springbootjunit4/core/entity/SimpleClassTest.java
+++ src/test/java/com/wedevol/springbootjunit4/core/entity/SimpleClassTest.java
@@ -1,13 +1,11 @@
 package com.wedevol.springbootjunit4.core.entity;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.util.Assert;
 
 public class SimpleClassTest {
 
     @Test
-    @DisplayName("Line array split not empty")
     public void lineArraySplitNotEmptyTest() {
         String line = "4567|arturo|perez|teleport engineering manager|Germany|Telecommunications|3|176";
         String[] lineParts = line.split("\\|");
